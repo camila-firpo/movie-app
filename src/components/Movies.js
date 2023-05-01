03cfb21497eb3a48a2086f6d95f88c99cd64be25
Camila <camilafirpo2294@hotmail.com>
2023-05-01 17:25:19 -0300

    commit inicial

---
+++ src/components/Movies.js
@@ -0,0 +1,19 @@
+<div className="container mt-3">
+<div className="row">
+  {movies.map((movie) => (
+    <div
+      key={movie.id}
+      className="col-md-4 mb-3"
+      onClick={() => selectMovie(movie)}
+    >
+      <img
+        src={`${URL_IMAGE + movie.poster_path}`}
+        alt=""
+        height={600}
+        width="100%"
+      />
+      <h4 className="text-center">{movie.title}</h4>
+    </div>
+  ))}
+</div>
+</div>
